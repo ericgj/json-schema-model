6dcd94e370bde3522638d61ba65626a597e89589
Eric Gjertsen <ericgj72@gmail.com>
2013-11-16 21:12:15 -0500

    tweak example, now custom non-null format not needed

--- test/integration/schemas/contact.js
+++ test/integration/schemas/contact.js
@@ -5,7 +5,7 @@ window.jsonSchemas['contact'] = {
   "default": { "name": null, "email": null, "phone": null },
   "required": ["name", "email"],
   "properties": {
-    "name":  { "format": "non-null", "minLength": 2 },
+    "name":  { "type": "string", "minLength": 2 },
     "email": { "type": "string", "default": null, "format": "email" },
     "phone": { "type": ["string","null"], "pattern": "^[0-9\-\.\s\(\)]+$" }
   }
